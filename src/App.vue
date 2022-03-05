edf1e41806eb3a6ca7a20e5e25d9b9002e26a3ba
skydev <skydev55@outlook.com>
2022-03-05 00:15:08 -0600

    use VueUse to toggle dark theme

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import MainMenu from "@/components/MainMenu.vue";
 import { VueQueryDevTools } from "vue-query/devtools";
 import { darkTheme } from 'naive-ui';
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 
 const userMenu = [
   {
@@ -18,26 +22,10 @@ const userMenu = [
     key: "logout",
   },
 ];
-
-const isDarkTheme = ref(localStorage.getItem('theme') === 'dark');
-
-if (localStorage.getItem('theme') === 'dark') {
-  document.documentElement.classList.add('dark')
-}
-
-const handleThemeChange = (isDark) => {
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
-  if (isDark) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
-}
-
 </script>
 
 <template>
-  <n-config-provider :theme="isDarkTheme ? darkTheme : null">
+  <n-config-provider :theme="isDark ? darkTheme : null">
     <n-loading-bar-provider>
       <n-message-provider placement="bottom">
         <n-notification-provider>
@@ -46,7 +34,7 @@ const handleThemeChange = (isDark) => {
               <div class="flex h-full w-full">
                 <aside class="flex flex-col sticky top-0 left-0 z-10 w-[60px] bg-[#202124]">
                   <MainMenu />
-                  <n-switch class="mt-auto mb-4" v-model:value="isDarkTheme"  @update:value="handleThemeChange"/>
+                  <n-switch class="mt-auto mb-4" v-model:value="isDarkTheme"  @update:value="toggleDark()"/>
                 </aside>
                 <section class="relative flex h-full w-full bg-[#F8F8FA]">
                   <router-view />
