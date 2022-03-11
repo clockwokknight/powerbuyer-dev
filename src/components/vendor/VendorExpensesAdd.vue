7c5af649edaf7d66ac079d7e2fd6f3c7783b3109
skydev <skydev55@outlook.com>
2022-03-11 17:51:37 -0600

    layout changed for create expense modal

--- src/components/vendor/VendorExpensesAdd.vue
+++ src/components/vendor/VendorExpensesAdd.vue
@@ -92,10 +92,7 @@ watch(
   () => form.value?.expenses,
   (newFormValue) => {
     if (newFormValue.length > 0) {
-      form.value.amount_due = newFormValue?.reduce(
-        (prev, curr) => prev + curr.amount,
-        0
-      );
+      form.value.amount_due = newFormValue?.reduce((prev, curr) => prev + curr.amount, 0);
     } else {
       form.value.amount_due = 0;
     }
@@ -176,8 +173,9 @@ const dealOptions = computed(() =>
 const searchVinSelect = ref("");
 const debouncedSearchVin = useDebounce(searchVinSelect, 500);
 
-const { data: searchDealResult, isLoading: isVendorSearchLoading } =
-  searchDealByVin(debouncedSearchVin);
+const { data: searchDealResult, isLoading: isVendorSearchLoading } = searchDealByVin(
+  debouncedSearchVin
+);
 const searchVinResultOptions = computed(() =>
   searchDealResult.value?.map((deal) => ({
     value: deal.id,
@@ -268,25 +266,25 @@ const onCreateExpenseItems = () => {
     </n-icon>
     Create Invoice
   </n-button>
-  <n-drawer v-model:show="showDrawer" :width="500">
-    <n-drawer-content title="Create Invoice">
-      <n-form
-        :model="form"
-        :rules="rules"
-        size="medium"
-        ref="formRef"
-        :disabled="isLoading"
+  <n-modal preset="card" class="custom-modal max-w-screen-md" v-model:show="showDrawer">
+    <n-form
+      :model="form"
+      :rules="rules"
+      size="medium"
+      ref="formRef"
+      :disabled="isLoading"
+    >
+      <div>Expenses</div>
+      <n-dynamic-input
+        v-model:value="form.expenses"
+        class="custom-dynamic-input my-5"
+        @create="onCreateExpenseItems"
+        #="{ index, value }"
+        show-sort-button
+        :min="1"
       >
-        <div>Expenses</div>
-        <n-dynamic-input
-          v-model:value="form.expenses"
-          class="custom-dynamic-input my-5"
-          @create="onCreateExpenseItems"
-          #="{ index, value }"
-          show-sort-button
-          :min="1"
-        >
-          <div class="rounded bg-gray-200/50 p-3 dark:bg-gray-800/50">
+        <div class="rounded bg-gray-200/50 p-3 dark:bg-gray-800/50">
+          <div class="sm:grid sm:grid-cols-2 sm:justify-between sm:gap-x-5">
             <n-form-item
               label="VIN"
               :path="`expenses[${index}].deal_id`"
@@ -304,41 +302,58 @@ const onCreateExpenseItems = () => {
               />
             </n-form-item>
             <n-form-item
+              label="Expense Date"
               ignore-path-change
-              :path="`expenses[${index}].name`"
-              :rule="rules.expenses.name"
-              label="Name"
+              :path="`expenses[${index}].expense_date`"
+              :rule="rules.expenses.expense_date"
             >
-              <n-select
-                :options="expenseItemsOptions"
-                filterable
-                @update-value="(val) => onExpenseSelect(val, index)"
-                :disabled="isLoading"
-                :loading="isLoading"
-                v-if="form.expenses[index].showSelect"
-              />
-              <n-input
-                v-else
-                v-model:value="form.expenses[index].name"
-                :loading="isLoading"
-                :disabled="isLoading"
-                @keydown.enter.prevent
+              <n-date-picker
+                v-model:formatted-value="form.expenses[index].expense_date"
+                value-format="yyyy-MM-dd"
               />
             </n-form-item>
-            <n-form-item label="Description">
-              <n-input
-                type="textarea"
-                v-model:value="form.expenses[index].description"
-                :loading="isLoading"
-                :disabled="isLoading"
-                @keydown.enter.prevent
-              />
-            </n-form-item>
+          </div>
+
+          <n-form-item
+            ignore-path-change
+            :path="`expenses[${index}].name`"
+            :rule="rules.expenses.name"
+            label="Name"
+            class="max-w-sm"
+          >
+            <n-select
+              :options="expenseItemsOptions"
+              filterable
+              @update-value="(val) => onExpenseSelect(val, index)"
+              :disabled="isLoading"
+              :loading="isLoading"
+              v-if="form.expenses[index].showSelect"
+            />
+            <n-input
+              v-else
+              v-model:value="form.expenses[index].name"
+              :loading="isLoading"
+              :disabled="isLoading"
+              @keydown.enter.prevent
+            />
+          </n-form-item>
+
+          <n-form-item label="Description">
+            <n-input
+              type="textarea"
+              v-model:value="form.expenses[index].description"
+              :loading="isLoading"
+              :disabled="isLoading"
+              @keydown.enter.prevent
+            />
+          </n-form-item>
+          <div class="sm:flex sm:gap-x-5">
             <n-form-item
               ignore-path-change
               :path="`expenses[${index}].expense_type_id`"
               :rule="rules.expenses.expense_type_id"
               label="Expense Type"
+              class="sm:w-3/4"
             >
               <n-select
                 :options="expenseTypeOptions"
@@ -354,6 +369,7 @@ const onCreateExpenseItems = () => {
               :path="`expenses[${index}].amount`"
               :rule="rules.expenses.amount"
               label="Amount"
+              class="sm:w-1/4"
             >
               <CurrencyInput
                 v-model="form.expenses[index].amount"
@@ -361,46 +377,35 @@ const onCreateExpenseItems = () => {
                 :disabled="isLoading"
               />
             </n-form-item>
-            <n-form-item
-              label="Expense Date"
-              ignore-path-change
-              :path="`expenses[${index}].expense_date`"
-              :rule="rules.expenses.expense_date"
-            >
-              <n-date-picker
-                v-model:formatted-value="form.expenses[index].expense_date"
-                value-format="yyyy-MM-dd"
-              />
-            </n-form-item>
           </div>
-        </n-dynamic-input>
+        </div>
+      </n-dynamic-input>
 
-        <n-form-item label="Amount Due" path="amount_due">
-          <currency-input
-            clearable
-            v-model="form.amount_due"
-            :loading="isLoading"
-            disabled
-          />
-        </n-form-item>
-        <n-form-item label="Due Date" path="due_date">
-          <n-date-picker
-            v-model:formatted-value="form.due_date"
-            value-format="yyyy-MM-dd"
-          />
-        </n-form-item>
-      </n-form>
-      <template #footer>
-        <n-button
-          :disabled="isLoading"
+      <n-form-item label="Amount Due" path="amount_due">
+        <currency-input
+          clearable
+          v-model="form.amount_due"
           :loading="isLoading"
-          size="large"
-          @click="submitExpense"
-          >Submit</n-button
-        >
-      </template>
-    </n-drawer-content>
-  </n-drawer>
+          disabled
+        />
+      </n-form-item>
+      <n-form-item label="Due Date" path="due_date">
+        <n-date-picker
+          v-model:formatted-value="form.due_date"
+          value-format="yyyy-MM-dd"
+        />
+      </n-form-item>
+    </n-form>
+    <template #footer>
+      <n-button
+        :disabled="isLoading"
+        :loading="isLoading"
+        size="large"
+        @click="submitExpense"
+        >Submit</n-button
+      >
+    </template>
+  </n-modal>
 </template>
 
 <style lang="scss">
