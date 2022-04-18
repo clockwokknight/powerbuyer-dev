113cb0299731d00ab981546c0d9716340a797e8f
skydev <skydev55@outlook.com>
2022-04-18 01:17:48 -0500

    WIP: dynamsoft integration

--- src/components/vehicle/Documents.vue
+++ src/components/vehicle/Documents.vue
@@ -1,11 +1,103 @@
 <script setup>
+
 import axios from "axios";
 import ActionButtons from "@/components/vehicle/DocumentActionButtons.vue";
 import { h, ref, computed, watch, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { utils, log } from "@/lib/utils";
 import { fetchById } from "@/hooks";
-import { useGlobalState } from "@/store/global";
+import Dynamsoft from "dwt";
+
+
+const containerId = "dwtControlContainer"
+const bWASM = ref(false);
+
+onMounted(()=>{
+  /**
+   * ResourcesPath & ProductKey must be set in order to use the library!
+   */
+  Dynamsoft.DWT.ResourcesPath = "dwt-resources";
+  Dynamsoft.DWT.ProductKey = "t00891wAAAKFs7VjcTP0UG20tzpw0mVsqmlIukOMDImLaclVr8l5ReM0df50rg9RNaH7A9mwLt6khlmvJyIqEixQeDZAz0iBvgzHPOXcQA/gbSOY51F46ANDILMM=";
+  Dynamsoft.DWT.Containers = [{
+    WebTwainId: "dwtObject",
+    ContainerId: containerId,
+    Width: "100%",
+    Height: "400px",
+  }, ];
+  Dynamsoft.DWT.RegisterEvent("OnWebTwainReady", () => {
+      Dynamsoft_OnReady();
+  });
+  Dynamsoft.DWT.Load();
+});
+
+let DWObject;
+let selectSources;
+/**
+ * Dynamsoft_OnReady is called when a WebTwain instance is ready to use.
+ * In this callback we do some initialization.
+ */
+const Dynamsoft_OnReady = ()=>{
+  DWObject = Dynamsoft.DWT.GetWebTwain(containerId);
+  bWASM.value = Dynamsoft.Lib.env.bMobile || !Dynamsoft.DWT.UseLocalService;
+  if (bWASM.value) {
+    DWObject.Viewer.cursor = "pointer";
+  } else {
+    let sources = DWObject.GetSourceNames();
+    selectSources = document.getElementById("sources");
+    selectSources.options.length = 0;
+    for (let i = 0; i < sources.length; i++) {
+      selectSources.options.add(new Option(sources[i], i.toString()));
+    }
+  }
+};
+/**
+ * Acquire images from scanners or cameras or local files
+ */
+const acquireImage = ()=>{
+    if (!DWObject) DWObject = Dynamsoft.DWT.GetWebTwain();
+    if (bWASM.value) {
+      alert("Scanning is not supported under the WASM mode!");
+    } else if (
+      DWObject.SourceCount > 0 &&
+      DWObject.SelectSourceByIndex(selectSources.selectedIndex)
+    ) {
+      const onAcquireImageSuccess = () => {
+        DWObject.CloseSource();
+      };
+      const onAcquireImageFailure = onAcquireImageSuccess;
+      DWObject.OpenSource();
+      DWObject.AcquireImage({},
+        onAcquireImageSuccess,
+        onAcquireImageFailure
+      );
+    } else {
+      alert("No Source Available!");
+    }
+};
+/**
+ * Open local images.
+ */
+const openImage = ()=>{
+    if (!DWObject) DWObject = Dynamsoft.DWT.GetWebTwain();
+    DWObject.IfShowFileDialog = true;
+    /**
+     * Note, this following line of code uses the PDF Rasterizer which is an extra add-on that is licensed seperately
+     */
+    DWObject.Addon.PDF.SetConvertMode(
+        Dynamsoft.DWT.EnumDWT_ConvertMode.CM_RENDERALL
+    );
+    DWObject.LoadImageEx(
+        "",
+        Dynamsoft.DWT.EnumDWT_ImageType.IT_ALL,
+        () => {
+            //success
+        },
+        () => {
+            //failure
+        }
+    );
+};
+
 
 const columns = [
   {
@@ -36,7 +128,6 @@ const columns = [
   },
 ];
 
-const global = useGlobalState();
 const route = useRoute();
 
 const { documents, isLoading } = {
@@ -55,25 +146,21 @@ const { documents, isLoading } = {
   isLoading: false,
 };
 
+const showScanModal = ref(false);
+
 const handleScan = () => {
+  showScanModal.value = true;
+};
 
-}
-
-const handleUpload = () => {
-
-}
+const handleUpload = () => {};
 </script>
 
 <template>
   <div
     class="rounded-round dark:bg-foreground_dark w-full border-[1px] bg-white p-[24px] dark:border-0"
   >
-    <n-button class="w-[220px]" @click="handleScan">
-      SCAN NEW
-    </n-button>
-    <n-button class="w-[220px]" @click="handleUpload">
-      UPLOAD NEW
-    </n-button>
+    <n-button class="w-[220px]" @click="handleScan"> SCAN NEW </n-button>
+    <n-button class="w-[220px]" @click="handleUpload"> UPLOAD NEW </n-button>
 
     <n-data-table
       class="rounded-round mt-[24px]"
@@ -86,6 +173,24 @@ const handleUpload = () => {
       :scroll-x="900"
     />
   </div>
+  <n-modal v-model:show="showScanModal">
+    <n-card
+      style="width: 600px"
+      title="Scan Document"
+      :bordered="false"
+      size="huge"
+      role="dialog"
+      aria-modal="true"
+    >
+      <template #header-extra> </template>
+      <!-- Content -->
+      <select v-if="!bWASM" id="sources"></select>
+      <button v-if="!bWASM" @click="acquireImage()">Acquire Images</button>
+      <button @click="openImage()">Open Documents</button>
+      <div :id="containerId"></div>
+      <template #footer> </template>
+    </n-card>
+  </n-modal>
 </template>
 
 <style scoped></style>
